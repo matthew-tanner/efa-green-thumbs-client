86b9ca09012af5e7b513f9d6aeab306b96de264f
Amie Hood <amiehood1@gmail.com>
2021-07-10 16:01:33 -0400

    fetch with api key

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ function App() {
     background: "#184D47",
   };
 
+  // const key = 'EsB6ufQhwYntOPZBgIhu6Jtf3jfbXjrAEvf8ZGEl'
+  // const baseUrl = 'https://developer.nps.gov/api/v1/events?'
+
+  const parkFetch = () => {
+    fetch ('https://developer.nps.gov/api/v1/events?stateCode=ME&api_key=EsB6ufQhwYntOPZBgIhu6Jtf3jfbXjrAEvf8ZGEl')
+    .then (res => res.json())
+    .then (data => console.log(data))
+  }
+  parkFetch()
+
+  const parkToDoFetch = () => {
+    fetch ('https://developer.nps.gov/api/v1/thingstodo?stateCode=ME&api_key=EsB6ufQhwYntOPZBgIhu6Jtf3jfbXjrAEvf8ZGEl')
+    .then (res => res.json())
+    .then (data2 => console.log(data2))
+  }
+  parkToDoFetch()
+
+
   return (
     <div className="App">
       <Row type="flex" justify="center" align="middle" style={{ minHeight: "100vh" }}>
