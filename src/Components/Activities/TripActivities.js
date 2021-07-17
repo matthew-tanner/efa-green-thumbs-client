ebe90981371787f4d3ad27a6497e81be380191a8
Janet Ziems <jsziems@gmail.com>
2021-07-17 12:18:34 -0400

    Mapped over activity data for cards. Currently <EditActivity /> is hardcoded with the first card in the array.

--- src/Components/Activities/TripActivities.js
+++ src/Components/Activities/TripActivities.js
@@ -3,134 +3,124 @@ import React, { useState } from 'react';
 import { Button, Card, Col, Row } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 
-import "./activities.css";
 import EditActivity from './EditActivity'
 
 
 // ToDo: Trip and activities should be passed in as parameters (props?)
 const TripActivities = () => {
 
-let tripActivityList = [
-{
-   id: 1,
-   name: "Boating",
-   description: "quiet lake",
-   cost: "Free",
-   notes: "looks like fun",
-   tripId: 1,
-   userId: 100
-}]
-console.log (tripActivityList[0].name)
+    let tripActivityList = [
+        {
+            id: 1,
+            name: "Boating",
+            description: "quiet lake",
+            cost: "$50/hour rental",
+            notes: "looks like fun",
+            tripId: 1,
+            userId: 100
+        },
+        {
+            id: 2,
+            name: "Hiking",
+            description: "Easy terrain",
+            cost: "Free",
+            notes: "",
+            tripId: 1,
+            userId: 100
+        },
+        {
+            id: 3,
+            name: "Mountain Biking",
+            description: "Advanced trail",
+            cost: "Free",
+            notes: "Scarry steep drop-offs",
+            tripId: 1,
+            userId: 100
+        },
+        {
+            id: 4,
+            name: "Nature Center",
+            description: "History of park, ranger programs, music",
+            cost: "Free",
+            notes: "Good recommendations for trails",
+            tripId: 1,
+            userId: 100
+        }
+    ]
+
+    console.log(tripActivityList[0].name)
     const [visible, setVisible] = useState(false);
     const { Meta } = Card;
 
+    // ToDo: delete activity from db and re-display the cards 
+    const handleDelete = () => {
+        console.log('In handleDelete -- DELETE THE ACTIVITY')
+    }
+
     return (
         <div className="site-card-wrapper" >
-            <div className="trip-activities">
+            <div style={{ margin: 20 }} >
                 <Row gutter={16}>
-                    <Col span={8}>
 
-                        <Card
-                            className="activityCard"
-                            cover={
-                                <img
-                                    alt="Park Planner Logo"
-                                    src="../../../assets/logo.png"
-                                />
-                            }
-                        >
-                            <Meta
-                                title={tripActivityList[0].name}
-                                bordered={true}
-                                description={tripActivityList[0].description}
-                            />
+                    {tripActivityList.map(tripActivity => {
+                        return (
+                            <Col span={8}>
+                                <Card
+                                    className="activityCard"
+                                    size="small"
+                                    style={{ width: 300, margin: 10 }}
+                                    cover={
+                                        <img
+                                            alt="Park Planner Logo"
+                                            src="../../../assets/logo.png"
+                                        />
+                                    }
+                                >
+                                    <Meta
+                                        title={tripActivity.name}
+                                        bordered={true}
+                                        description={tripActivity.description}
+                                    />
 
-                            <Button
-                                icon={<EditOutlined />}
-                                onClick={() => { setVisible(true) }}
-                            />
+                                    <Button
+                                    // ToDo:  ASSIGN/SET VARIABLES SO THEY"RE VISIBLE IN EditActivity ???
+                                        icon={<EditOutlined />}
+                                        onClick={() => { setVisible(true) }}
+                                    />
 
-                            <Button
-                                shape="circle"
-                                style={{ color: 'black', zIndex: 10 }}
-                                //   stopPropogation rec from https://stackoverflow.com/questions/61040013/ant-design-how-can-i-handle-button-in-card-extra-onclick-event
-                                onClick={e => { e.stopPropagation(); console.log('DELETE THE ACTIVITY') }}
-                            >
-                                X
-                            </Button>
-                        </Card>
-                    </Col>
+                                    <Button
+                                        shape="circle"
+                                        style={{ color: 'black', zIndex: 10 }}
+                                        //   stopPropogation rec from https://stackoverflow.com/questions/61040013/ant-design-how-can-i-handle-button-in-card-extra-onclick-event
+                                        onClick={e => { e.stopPropagation(); handleDelete() }}
+                                    >
+                                        X
+                                    </Button>
+                                    {/* POPULATES MODAL FORM WITH DATA FROM THE LAST CARD INSTEAD OF THE CARD BEING CLICKED */}
+                                    {/* <EditActivity
+                                        tripActivity={tripActivity}
+                                        visible={visible}
+                                        setVisible={setVisible}
+                                    /> */}
+                                </Card>
+                            </Col>
+                        )
+                    })}
 
-                    <Col span={8}>
-                        <Card
-                            className="activityCard"
-                            cover={
-                                <img
-                                    alt="Park Planner Logo"
-                                    src="../../../assets/logo.png"
-                                />
-                            }
-                        >
-                            <Meta
-                                title="PUT NEXT ACTIVITY NAME HERE"
-                                bordered={true}
-                                description="PUT MORE ACTIVITY INFO HERE"
-                            />
-
-                            <Button
-                                icon={<EditOutlined />}
-                                onClick={() => { setVisible(true) }}
-                            />
-
-                            <Button
-                                shape="circle"
-                                style={{ color: 'black', zIndex: 10 }}
-                                //   stopPropogation rec from https://stackoverflow.com/questions/61040013/ant-design-how-can-i-handle-button-in-card-extra-onclick-event
-                                onClick={e => { e.stopPropagation(); console.log('DELETE THE ACTIVITY') }}
-                            >
-                                X
-                            </Button>
-                        </Card>
-                    </Col>
-
-                    <Col span={8}>
-                        <Card
-                            className="activityCard"
-                            cover={
-                                <img
-                                    alt="Park Planner Logo"
-                                    src="../../../assets/logo.png"
-                                />
-                            }
-                        >
-                            <Meta
-                                title="THIRD ACTIVITY"
-                                bordered={true}
-                                description="ACTIVITY INFO HERE"
-                            />
-                                                        <Button
-                                icon={<EditOutlined />}
-                                onClick={() => { setVisible(true) }}
-                            />
-
-                            <Button
-                                shape="circle"
-                                style={{ color: 'black', zIndex: 10 }}
-                                //   stopPropogation rec from https://stackoverflow.com/questions/61040013/ant-design-how-can-i-handle-button-in-card-extra-onclick-event
-                                onClick={e => { e.stopPropagation(); console.log('DELETE THE ACTIVITY') }}
-                            >
-                                X
-                            </Button>
-                        </Card>
-
-                    </Col>
                 </Row>
             </div>
-            <EditActivity 
-                tripActivity={tripActivityList[0]} 
-                visible={visible} 
-                setVisible={setVisible} 
+            {/* DATA IS ON THE FORM, BUT ISN"T TIED TO THE MAPPED DATA */}
+            <EditActivity
+                tripActivity={tripActivityList[0]}
+                visible={visible}
+                setVisible={setVisible}
             />
+            {/* {tripActivity} IS UNDEFINED HERE
+            <EditActivity
+                tripActivity={tripActivity}
+                visible={visible}
+                setVisible={setVisible}
+            /> */}
 
         </div>
 
--- src/Components/Activities/scratch.js
+++ src/Components/Activities/scratch.js
@@ -1,28 +1,45 @@
+import { formatCountdown } from "antd/lib/statistic/utils"
+
 const handleOk = () => {
     console.log('In handleOk -- DO SOMETHING WITH DATA THE USER ENTERED')
     console.log(notes)
 
     fetch(`http://localhost:3001/activity/${id}`, {
         method: 'PUT',
-        headers: new Headers ({
+        headers: new Headers({
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${props.token}`
-            }),
-            body: JSON.stringify({notes: notes})
-        })
+        }),
+        body: JSON.stringify({ notes: notes })
+    })
         .then(res => res.json())
         .then(activityData => {
             console.log(activityData)
+            // Want to remove the data from the form, but don't want to update the properties.  I don't think this code is correct.
             setName('')
             setDescription('')
             setCost('')
             setNotes('')
-// Re-Display the activity cards?  Not sure if this is how to do it.  Would need to pass the function into EditActivity as a prop.
+            // Re-Display the activity cards?  Not sure if this is how to do it.  Would need to pass the function into EditActivity as a prop.
             props.TripActivities()
         })
         .catch(err => {
             console.error(err)
         })
 
-        props.setVisible(false);
-    }
+    props.setVisible(false);
+}
+
+
+const handleCancel = () => {
+    console.log('In handleCancel')
+    // Want to remove the data from the form, but don't want to update the properties.  I don't think this code is correct.
+    setName('')
+    setDescription('')
+    setCost('')
+    setNotes('')
+    // Re-Display the activity cards?  Not sure if this is how to do it.  Would need to pass the function into EditActivity as a prop.
+    props.TripActivities()
+
+    props.setVisible(false);
+};
